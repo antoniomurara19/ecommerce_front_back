34133cfe366bb8bdc5da187a70b3ff812d28dacd
ANTÔNIO MURARA ZABALA <antonio_zabala@estudante.sesisenai.org.br>
2023-11-14 12:21:12 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const e = require('express')
 const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
@@ -19,6 +20,19 @@ app.use(express.static('public'))
 app.set('view engine', 'handlebars')
 app.engine('handlebars', handlebars.engine())
 // =================================================
+app.post('/register', async (req,res)=>{
+    const nome = req.body.nome
+    const email = req.body.email
+    const telefone = req.body.telefone
+    const cpf = req.body.cpf
+    const senha = req.body.senha
+    const tipo = req.body.tipo
+    
+    await Usuario.create({nome:nome, email:email, telefone:telefone, cpf:cpf, senha:senha, tipo:tipo})
+    let msg = 'Dados Cadastrados'
+    res.render('login', {log, user, tipo_user})
+})
+// =================================================
 app.post('/login', async (req,res)=>{
     const email = req.body.email
     const senha = req.body.senha
@@ -27,49 +41,53 @@ app.post('/login', async (req,res)=>{
     let msg = 'Usuário não Cadastrado'
 
     if(pesq_login == null){
-        res.render('login', {msg})
-    }else if(email == pesq_login.email && senha == pesq_login.senha && pesq_login.tipo === 'gerente'){
+        user = pesq_login.nome
+        tipo_user = pesq_login.tipo
+        res.render('login', {msg, user, tipo_user})
+    }else if(email == pesq_login.email && senha == pesq_login.senha && pesq_login.tipo == 'gerente' || pesq_login.tipo == 'Gerente'){
         log = true
         user = pesq_login.nome
         tipo_user = pesq_login.tipo
         res.render('gerenciador', {log, user, tipo_user})        
-    }else if(email == pesq_login.email && senha == pesq_login.senha && pesq_login.tipo === 'cliente'){
+    }else if(email == pesq_login.email && senha == pesq_login.senha && pesq_login.tipo == 'cliente' || pesq_login.tipo == 'Cliente'){
         log = true
-        user = pesq_login.user
+        user = pesq_login.nome
         tipo_user = pesq_login.tipo
         res.render('home', {log, user, tipo_user})
     }else{
-        res.render('home', {msg})
+        user = pesq_login.nome
+        tipo_user = pesq_login.tipo
+        res.render('home', {msg, user, tipo_user})
     }
 })
 // ============= renderizando ======================
 app.get('/planetas',(req,res)=>{
-    res.render('planetas',{log})
+    res.render('planetas',{log, user, tipo_user})
 })
 app.get('/logout',(req,res)=>{
     let log = false
-    res.render('home',{log})
+    res.render('home',{log, user, tipo_user})
 })
 app.get('/excluir_viagem',(req,res)=>{
-    res.render('excluir_viagem',{log})
+    res.render('excluir_viagem',{log, user, tipo_user})
 })
 app.get('/listar_viagem',(req,res)=>{
-    res.render('listar_viagem',{log})
+    res.render('listar_viagem',{log, user, tipo_user})
 })
 app.get('/cadastrar_viagem',(req,res)=>{
-    res.render('cadastrar_viagem',{log})
+    res.render('cadastrar_viagem',{log, user, tipo_user})
 })
 app.get('/editar_viagem',(req,res)=>{
-    res.render('editar_viagem',{log})
+    res.render('editar_viagem',{log, user, tipo_user})
 })
 app.get('/register',(req,res)=>{
-    res.render('register',{log})
+    res.render('register',{log, user, tipo_user})
 })
 app.get('/login',(req,res)=>{
-    res.render('login',{log})
+    res.render('login',{log, user, tipo_user})
 })
 app.get('/',(req,res)=>{
-    res.render('home',{log})
+    res.render('home',{log, user, tipo_user})
 })
 // =================================================
 conn.sync().then(()=>{
